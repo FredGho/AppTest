d688b5d5d21a50cab9ac902cedff9cf957711826
BriandFMarie <briand.marie@live.fr>
2020-04-29 10:05:59 +0200

    [Configuration] - add hook pre-commit and code coverage

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,19 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.{js, vue}'],
+  coverageReporters: [
+    'html',
+    'lcovonly',
+    'text-summary',
+  ],
+  coverageDirectory: './coverage',
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70,
+    },
+  },
 };
