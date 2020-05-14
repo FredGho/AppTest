531b16a5217a36db2f4950d16f90bf4f67ac0d03
FredGho <frederic.gueho@imie.fr>
2020-05-14 12:26:11 +0200

    modif home.vue 2

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your GROSS_HACKER App"/>
+    <HelloWorld msg="Welcome to Your GROSS_HACKER App hihi"/>
   </div>
 </template>
 
