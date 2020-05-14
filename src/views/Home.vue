9b8f38bb115b144f72ed6ae15b2f6c05c7b8ed1c
FredGho <frederic.gueho@imie.fr>
2020-05-14 13:50:27 +0200

    test1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your GROSS_HACKER App hihihehe"/>
+    <HelloWorld msg="Welcome to Your GROSS_HACKER App test1"/>
   </div>
 </template>
 
